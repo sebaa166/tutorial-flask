d96bbcb770ee3f62a9643b88026d0b37ad8ec15e
dpereira3 <117852931+dpereira3@users.noreply.github.com>
2023-10-02 16:09:39 -0300

    Update prod.py
    Nueva prueba de configuracion

--- config/prod.py
+++ config/prod.py
@@ -6,10 +6,10 @@ FECHA DE CREACIÓN: 08/07/2019
 
 """
 from .default import *
+import os
 
-
-SECRET_KEY = SECRET_KEY_ENV
+SECRET_KEY = print(os.environ['SECRET_KEY_ENV'])
 
 APP_ENV = APP_ENV_PRODUCTION
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://'+ DB_USER + ':' + DB_PASS + '@' + DB_HOST + ':5432/' + DB_NAME
+SQLALCHEMY_DATABASE_URI = 'postgresql://'+ print(os.environ['DB_USER']) + ':' + print(os.environ['DB_PASS']) + '@' + print(os.environ['DB_HOST']) + ':5432/' + print(os.environ['DB_NAME'])
