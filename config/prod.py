e69ac9f7efdb9a11ee73e1f6a8574b07c250d72b
dpereira3 <117852931+dpereira3@users.noreply.github.com>
2023-10-02 14:25:11 -0300

    Update prod.py
    Agregadas variables de entorno produccion.

--- config/prod.py
+++ config/prod.py
@@ -8,8 +8,8 @@ FECHA DE CREACIÓN: 08/07/2019
 from .default import *
 
 
-SECRET_KEY = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95cae409aebd47f7'
+SECRET_KEY = SECRET_KEY_ENV
 
 APP_ENV = APP_ENV_PRODUCTION
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_pass@host:port/db_name'
+SQLALCHEMY_DATABASE_URI = 'postgresql://'+ DB_USER + ':' + DB_PASS + '@' + DB_HOST + ':5432/' + DB_NAME
