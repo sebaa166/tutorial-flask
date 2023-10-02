450cd863daf0b84bb923005f21616dbf6e32d490
dpereira3 <117852931+dpereira3@users.noreply.github.com>
2023-10-02 17:19:49 -0300

    Update prod.py
    prueba get env

--- config/prod.py
+++ config/prod.py
@@ -8,8 +8,8 @@ FECHA DE CREACIÓN: 08/07/2019
 from .default import *
 import os
 
-SECRET_KEY = os.getenv(['SECRET_KEY_ENV'])
+SECRET_KEY = os.getenv('SECRET_KEY_ENV')
 
 APP_ENV = APP_ENV_PRODUCTION
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://'+ os.getenv(['DB_USER']) + ':' + os.getenv(['DB_PASS']) + '@' + os.getenv(['DB_HOST']) + ':5432/' + os.getenv(['DB_NAME'])
+SQLALCHEMY_DATABASE_URI = 'postgresql://'+ os.getenv('DB_USER') + ':' + os.getenv('DB_PASS') + '@' + os.getenv('DB_HOST') + ':5432/' + os.getenv('DB_NAME')
