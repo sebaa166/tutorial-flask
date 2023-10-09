ec6c0698e0747db627936eb794352ae9829cf496
sebaa166 <134320592+sebaa166@users.noreply.github.com>
2023-10-09 17:44:05 -0300

    Update prod.py
    1

--- config/prod.py
+++ config/prod.py
@@ -12,4 +12,4 @@ SECRET_KEY = os.getenv('SECRET_KEY_ENV')
 
 APP_ENV = APP_ENV_PRODUCTION
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://'+ os.getenv('DB_USER') + ':' + os.getenv('DB_PASS') + '@' + os.getenv('DB_HOST') + ':5432/' + os.getenv('DB_NAME')
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
